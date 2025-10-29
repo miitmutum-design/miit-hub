e24b284368b03b92dbabf038617c2e1105baeae5
MiiT <miit.mutum@gmail.com>
2025-10-29 01:37:23 +0000

    coloque os * para o usuario entender que é obrigatório (_for element <Pr

--- src/components/common/LoginModal.tsx
+++ src/components/common/LoginModal.tsx
@@ -23,7 +23,7 @@ import { useToast } from "@/hooks/use-toast";
 
 // Simple SVG for Google Icon
 const GoogleIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" {...props}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24" {...props}>
     <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
     <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
     <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/>
@@ -116,7 +116,8 @@ export default function LoginModal({ isOpen, onOpenChange, onLoginSuccess }: Log
         {showEmailLogin ? (
           // Email Login Form
           <div className="py-4 space-y-4">
-            <div className="flex flex-col items-center space-y-4">
+            <div className="flex flex-col items-center space-y-2">
+                <Label className='mb-2'>Foto de Perfil <span className="text-destructive">*</span></Label>
                 <div className="relative inline-block">
                 <Avatar className="h-24 w-24 border-2 border-dashed border-border">
                     {avatarUrl ? (
@@ -151,15 +152,15 @@ export default function LoginModal({ isOpen, onOpenChange, onLoginSuccess }: Log
             </div>
 
             <div className="space-y-2">
-                <Label htmlFor="name">Nome</Label>
+                <Label htmlFor="name">Nome <span className="text-destructive">*</span></Label>
                 <Input id="name" type="text" placeholder="Seu nome completo" value={name} onChange={(e) => setName(e.target.value)} required />
             </div>
             <div className="space-y-2">
-                <Label htmlFor="email">Email</Label>
+                <Label htmlFor="email">Email <span className="text-destructive">*</span></Label>
                 <Input id="email" type="email" placeholder="seu@email.com" value={email} onChange={(e) => setEmail(e.target.value)} required />
             </div>
             <div className="space-y-2">
-                <Label htmlFor="password">Senha</Label>
+                <Label htmlFor="password">Senha <span className="text-destructive">*</span></Label>
                 <Input id="password" type="password" placeholder="********" value={password} onChange={(e) => setPassword(e.target.value)} required />
             </div>
             <DialogFooter className='pt-4'>
